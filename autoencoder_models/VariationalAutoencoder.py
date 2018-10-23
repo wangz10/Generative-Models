1ee26730e6a431a4e60a817809f28f4d128853ed
wangz10 <wangzc921@gmail.com>
2018-10-23 16:01:36 -0400

    Implemented save and load methods for VAE

--- autoencoder_models/VariationalAutoencoder.py
+++ autoencoder_models/VariationalAutoencoder.py
@@ -1,53 +1,63 @@
+import os
+import json
+import numpy as np
 import tensorflow as tf
 
 class VariationalAutoencoder(object):
 
-    def __init__(self, n_layers, transfer_function=tf.nn.softplus, optimizer = tf.train.AdamOptimizer()):
+    def __init__(self, n_layers=[784, 10], learning_rate=0.001):
         self.n_layers = n_layers
-        self.transfer = transfer_function
+        self.learning_rate = learning_rate
 
-        network_weights = self._initialize_weights()
-        self.weights = network_weights
+        self.transfer = tf.nn.softplus 
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            optimizer = tf.train.AdamOptimizer(learning_rate)
+            network_weights = self._initialize_weights()
+            self.weights = network_weights
 
-        # model
-        self.x = tf.placeholder(tf.float32, [None, self.n_layers[0]])
-        self.hidden_encode = []
-        h = self.x
-        for layer in range(len(self.n_layers)-2):
-            h = self.transfer(
-                tf.add(tf.matmul(h, self.weights['encode'][layer]['w']),
-                       self.weights['encode'][layer]['b']))
-            self.hidden_encode.append(h)
-            
+            # model
+            self.x = tf.placeholder(tf.float32, [None, self.n_layers[0]])
+            self.hidden_encode = []
+            h = self.x
+            for layer in range(len(self.n_layers)-2):
+                h = self.transfer(
+                    tf.add(tf.matmul(h, self.weights['encode'][layer]['w']),
+                        self.weights['encode'][layer]['b']))
+                self.hidden_encode.append(h)
+                
 
-        self.z_mean = tf.add(tf.matmul(h, self.weights['mean']['w']), self.weights['mean']['b'])
-        self.z_log_sigma_sq = tf.add(tf.matmul(h, self.weights['log_sigma']['w']), self.weights['log_sigma']['b'])
+            self.z_mean = tf.add(tf.matmul(h, self.weights['mean']['w']), self.weights['mean']['b'])
+            self.z_log_sigma_sq = tf.add(tf.matmul(h, self.weights['log_sigma']['w']), self.weights['log_sigma']['b'])
 
-        # sample from gaussian distribution
-        eps = tf.random_normal(tf.stack([tf.shape(self.x)[0], self.n_layers[-1]]), 0, 1, dtype = tf.float32)
-        self.z = tf.add(self.z_mean, tf.multiply(tf.sqrt(tf.exp(self.z_log_sigma_sq)), eps))
+            # sample from gaussian distribution
+            eps = tf.random_normal(tf.stack([tf.shape(self.x)[0], self.n_layers[-1]]), 0, 1, dtype = tf.float32)
+            self.z = tf.add(self.z_mean, tf.multiply(tf.sqrt(tf.exp(self.z_log_sigma_sq)), eps))
 
-        self.hidden_recon = []
-        h = self.z
-        for layer in range(len(self.n_layers)-1):
-            h = self.transfer(
-                tf.add(tf.matmul(h, self.weights['recon'][layer]['w']),
-                       self.weights['recon'][layer]['b']))
-            self.hidden_recon.append(h)
-        self.reconstruction = self.hidden_recon[-1]
+            self.hidden_recon = []
+            h = self.z
+            for layer in range(len(self.n_layers)-1):
+                h = self.transfer(
+                    tf.add(tf.matmul(h, self.weights['recon'][layer]['w']),
+                        self.weights['recon'][layer]['b']))
+                self.hidden_recon.append(h)
+            self.reconstruction = self.hidden_recon[-1]
 
-        # cost
-        # squared loss
-        reconstr_loss = 0.5 * tf.reduce_sum(tf.pow(tf.subtract(self.reconstruction, self.x), 2.0))
-        # KL divergence
-        latent_loss = -0.5 * tf.reduce_sum(1 + self.z_log_sigma_sq
-                                           - tf.square(self.z_mean)
-                                           - tf.exp(self.z_log_sigma_sq), 1)
-        self.cost = tf.reduce_mean(reconstr_loss + latent_loss)
-        self.optimizer = optimizer.minimize(self.cost)
+            # cost
+            # squared loss
+            reconstr_loss = 0.5 * tf.reduce_sum(tf.pow(tf.subtract(self.reconstruction, self.x), 2.0))
+            # KL divergence
+            latent_loss = -0.5 * tf.reduce_sum(1 + self.z_log_sigma_sq
+                                            - tf.square(self.z_mean)
+                                            - tf.exp(self.z_log_sigma_sq), 1)
+            self.cost = tf.reduce_mean(reconstr_loss + latent_loss)
+            self.optimizer = optimizer.minimize(self.cost)
 
-        init = tf.global_variables_initializer()
-        self.sess = tf.Session()
+            # create a saver 
+            self.saver = tf.train.Saver()
+            init = tf.global_variables_initializer()
+        
+        self.sess = tf.Session(graph=self.graph)
         self.sess.run(init)
 
     def _initialize_weights(self):
@@ -99,15 +109,36 @@ class VariationalAutoencoder(object):
     def transform(self, X):
         return self.sess.run(self.z_mean, feed_dict={self.x: X})
 
-    def transform_z(self, X):
-        return self.sess.run(self.z, feed_dict={self.x: X})
-
     def generate(self, hidden = None):
         if hidden is None:
-            hidden = self.sess.run(tf.random_normal([1, self.n_layers[-1]]))
+            hidden = np.random.randn(1, self.n_layers[-1])
         return self.sess.run(self.reconstruction, feed_dict={self.z: hidden})
 
     def reconstruct(self, X):
         return self.sess.run(self.reconstruction, feed_dict={self.x: X})
 
+    def save(self, path):
+        '''To save trained model and its params.
+        '''
+        save_path = self.saver.save(self.sess, 
+            os.path.join(path, 'model.ckpt'))
+        # save parameters of the model
+        params = {'n_layers': self.n_layers, 'learning_rate': self.learning_rate}
+        json.dump(params, 
+            open(os.path.join(path, 'model_params.json'), 'w'))
+        return save_path
 
+    def _restore(self, path):
+        with self.graph.as_default():
+            self.saver.restore(self.sess, path)
+
+    @classmethod
+    def load(cls, path):
+        '''To restore a saved model.
+        '''
+        # load params of the model
+        params = json.load(open(os.path.join(path, 'model_params.json'), 'r'))
+        # init an instance of this class
+        estimator = cls(**params)
+        estimator._restore(os.path.join(path, 'model.ckpt'))
+        return estimator
